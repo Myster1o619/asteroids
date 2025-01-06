18543c7d6669bba4cb193624075fbfd862d65b87
Myster1o619 <myster1o619@hotmail.com>
2025-01-06 09:39:04 +0200

    add additional notes to help with understanding

--- circleshape.py
+++ circleshape.py
@@ -9,8 +9,8 @@ class CircleShape(pygame.sprite.Sprite):
         else:
             super().__init__()
 
-        self.position = pygame.Vector2(x, y)
-        self.velocity = pygame.Vector2(0, 0)
+        self.position = pygame.Vector2(x, y) # Starting position of the circle
+        self.velocity = pygame.Vector2(0, 0) # Speed and direction
         self.radius = radius
 
     def draw(self, screen):
