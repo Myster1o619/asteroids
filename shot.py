b713aa7285b7e68cde55b129d05ef35a860bb37b
Myster1o619 <myster1o619@hotmail.com>
2025-01-07 13:05:02 +0200

    add initial logic for bullet/shot class

---
+++ shot.py
@@ -0,0 +1,16 @@
+import pygame
+from circleshape import CircleShape
+from constants import SHOT_RADIUS
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.position.x = x
+        self.position.y = y
+        self.radius = SHOT_RADIUS
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", (int(self.position.x), int(self.position.y)), self.radius, 7)
+
+    def update(self, dt):
+        self.position += self.velocity * dt # Update the position using velocity and delta
