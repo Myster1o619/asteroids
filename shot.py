ad540377a8db71a3fdba77bacd67699c6b55c96c
Myster1o619 <myster1o619@hotmail.com>
2025-01-07 15:19:50 +0200

    change bullet color to red

--- shot.py
+++ shot.py
@@ -2,7 +2,7 @@ import pygame
 from circleshape import CircleShape
 from constants import SHOT_RADIUS
 
-class Asteroid(CircleShape):
+class Shot(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
         self.position.x = x
@@ -10,7 +10,7 @@ class Asteroid(CircleShape):
         self.radius = SHOT_RADIUS
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", (int(self.position.x), int(self.position.y)), self.radius, 1)
+        pygame.draw.circle(screen, "red", (int(self.position.x), int(self.position.y)), self.radius, 1)
 
     def update(self, dt):
-        self.position += self.velocity * dt # Update the position using velocity and delta
+        self.position += self.velocity * dt
