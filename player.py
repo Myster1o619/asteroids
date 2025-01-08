d32dfb32831480a3d9374924e1f3f4d4c1459d42
Myster1o619 <myster1o619@hotmail.com>
2025-01-08 13:23:35 +0200

    modify update method to adjust time allowed between valid player shots

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED, PLAYER_SHOOT_SPEED, SHOT_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_SHOOT_COOLDOWN, PLAYER_SPEED, PLAYER_TURN_SPEED, PLAYER_SHOOT_SPEED, SHOT_RADIUS
 from circleshape import CircleShape
 from shot import Shot
 
@@ -7,6 +7,8 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0.00
+        self.first_shot_fired = False
 
     # in the player class
     def triangle(self):
@@ -35,18 +37,33 @@ class Player(CircleShape):
         if keys[pygame.K_d]:
             self.rotate(dt)
         if keys[pygame.K_SPACE]:
-            new_bullet = self.shoot(dt)
-            new_bullet.update(dt)
+            if not self.first_shot_fired:
+                self.shoot_timer = self.set_player_shoot_timer()
+                self.first_shot_fired = True
+                self.__create_and_update_bullet(dt)
+            elif self.first_shot_fired and self.shoot_timer <= 0:
+                self.shoot_timer = self.set_player_shoot_timer()
+                self.__create_and_update_bullet(dt)
+            else:
+                pass
+            self.shoot_timer -= dt
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
-    def shoot(self, dt):
+    def shoot(self):
         new_shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
         forward = pygame.Vector2(0, 1)
         direction = forward.rotate(self.rotation)
         velocity = direction * PLAYER_SHOOT_SPEED
         new_shot.velocity = velocity
         return new_shot
+    
+    def __create_and_update_bullet(self, dt):
+        new_bullet = self.shoot()
+        new_bullet.update(dt)
+
+    def set_player_shoot_timer(self):
+        return PLAYER_SHOOT_COOLDOWN
         
