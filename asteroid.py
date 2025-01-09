8ea273eb53c609b3e2f7ebe4979c6fe6cc47f86d
Myster1o619 <myster1o619@hotmail.com>
2025-01-09 07:29:46 +0200

    add note for self.radius check

--- asteroid.py
+++ asteroid.py
@@ -17,7 +17,7 @@ class Asteroid(CircleShape):
 
     def split(self):
         self.kill()
-        if self.radius == ASTEROID_MIN_RADIUS:
+        if self.radius == ASTEROID_MIN_RADIUS: # Small asteroid hit, don't spawn new asteroids
             return
         else:
             asteroid_split_angle = self.__generate_random_angle()
